9d439283940b0be4ab510b3ac2b4c98cccb472e1
159159951 <quanghuynguoiyeucuanguoiyeu@yahoo.com>
2015-05-21 11:16:42 +0700

    Upload new Assert
    Soure code, database, report

--- www/index.php
+++ www/index.php
@@ -1,16 +1,29 @@
-<?php 
-session_start();
-?>
-
+<!DOCTYPE html>
 <html>
-<header>
-<meta http-equiv="Content-Type" content = "text/html; charset = utf8" />
-</header>
+<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<link href="css\css-v=s0sTbq4cCKHhaWgXBsoPjjc9d-zjr1P1RNAaeFhDWbc1.css" rel="stylesheet" />
+		<link href="css\listproduct.css-v=3110.css" rel="stylesheet" />
+		<link href="css\fixlistproduct.css-v=3110.css" rel="stylesheet" />
+		<link href="css\jquery.jcarousel.css-v=3110.css" rel="stylesheet" />
+		<link href="css\breadcrumbs.css-v=3110.css" rel="stylesheet" />
+		<link href="css\style1.css" rel="stylesheet"/>
+		<link href="css\style2.css" rel="stylesheet"/>
+		<link href="css\style3.css" rel="stylesheet"/>
+		<link href="css\style4.css" rel="stylesheet"/>
+		<link href="css\bootstrap.min.css" rel="stylesheet"/>
+		<script type="text/javascript" src="js\jquery-1.9.1.min.js"></script>
+		<script type="text/javascript" src="js\jssor.js"></script>
+		<script type="text/javascript" src="js\jssor.slider.js"></script>
+		
+</head>
 <body>
-<h1>Xin chào mừng <?php echo $_SESSION['user'];?> đã đăng nhập vào hệ thống.!</h1>
-
-
-
-
+	<?php 
+	
+	header("Location: htdoc/products.php");
+//		require("htdoc/products.php");
+	?>
 </body>
-</html
+</html>
+
+
